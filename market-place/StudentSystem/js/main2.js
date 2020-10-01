d22832b7dce6e4b9edf178806cdfd2ad2fe7f7f3
leon <leon@localhost.localdomain>
2020-10-01 17:29:10 +0200

    add front

--- market-place/StudentSystem/js/main2.js
+++
@@ -1,7 +0,0 @@
-$(document).ready( function () {
-    $('#table_id').DataTable();
-    $('#table_id').on('click', '.btnDelete', function () {
-        $(this).closest('tr').remove();
-    });
-} );
-// setInterval(function(){alert("Hello")},3000);
